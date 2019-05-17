2212d444c13c280aef8907208d981222e92fae9d
Alessandrode Sousa Rocha <alessandro.s.rocha@hotmail.com>
2019-05-16 21:35:27 -0300

    conclusão do testes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  Photo from './components/Photo';
 
 function App() {
+ 
+  const imageData = {
+    src: "https://images.unsplash.com/photo-1508182314998-3bd49473002f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=af394daae4bdbc4a95dc2204984b790d&auto=format&fit=crop&w=800&q=60",
+    user: {
+      name: "Alessandro Rocha",
+      profile_image: "https://avatars3.githubusercontent.com/u/16439783?s=40&v=4",
+      link: "https://www.linkedin.com/in/alessandro-de-sousa-rocha-743b14a4/"
+    }
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Photo {...imageData} />
     </div>
   );
 }
 
+
 export default App;
+
